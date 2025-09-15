eb585129220cff6691dd034c5bc4cb9c17ad46ad
G6B0 <146021873+G6B0@users.noreply.github.com>
2025-09-15 17:10:41 -0300

    Se creó TowerSketch con .h
    Se creó TowerSketch para usarse con CountMin con Conservative Update inicialmente funcional con valores de enteros.

--- TowerSketch.cpp
+++ TowerSketch.cpp
@@ -1,18 +1,33 @@
-#include "CountMin_CU.cpp"
+#include "TowerSketch.h"
 
-class TowerSketch
-{
-private:
-    int niveles;
-    std::vector<CountMin_CU> Tower;
-    
-public:
-    TowerSketch(int niveles):
-    niveles(niveles), Tower(niveles){
+TowerSketch::TowerSketch(int niveles):
+    niveles(niveles){
+        tower_create(niveles);
+}
 
+void TowerSketch::tower_create(int niveles){
+    int j = 2;
+    Tower.clear();
+    Tower.reserve(niveles);
+    for(int i = 0; i < niveles; i++){
+        Tower.emplace_back(5,j);
+        j *= 2; 
     }
-
-};
-
-
-
+}
+    
+void TowerSketch::insert(uint64_t elemento){
+    for(int i = 0; i < niveles; i++){
+        Tower[i].insertCMin(elemento);
+    }
+}
+    
+double TowerSketch::estimar_freq(uint64_t elemento){
+    int freq_est = INT_MAX;
+    for(int i = 0; i < niveles; i++){
+        int val = Tower[i].estimar_freq(elemento); 
+        if(val < freq_est){
+            freq_est = val;
+        }
+    }
+    return freq_est;
+}
