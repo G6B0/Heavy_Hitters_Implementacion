a160b0816d452ab8b01f3a7e4e3e5e83a54a5daf
G6B0 <146021873+G6B0@users.noreply.github.com>
2025-09-14 22:14:57 -0300

    Creación de TowerSketch.cpp
    Se creó la clase TowerSketch, inicialmente solo se ha declarado, no se ha implementado.

---
+++ TowerSketch.cpp
@@ -0,0 +1,18 @@
+#include "CountMin_CU.cpp"
+
+class TowerSketch
+{
+private:
+    int niveles;
+    std::vector<CountMin_CU> Tower;
+    
+public:
+    TowerSketch(int niveles):
+    niveles(niveles), Tower(niveles){
+
+    }
+
+};
+
+
+
