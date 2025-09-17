f365a09ae4ba8b1166cb89ef99d8ecd57f685ddf
G6B0 <146021873+G6B0@users.noreply.github.com>
2025-09-17 20:52:36 -0300

    Main para probar los distintos Sketchs realizados
    Main para poder ejecutar los sketchs, funciona muy bien.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include "TowerSketch.h"
-
-int main (int argc, char *argv[]) {
-    TowerSketch TowerSK(5);
-    for(int j=0; j < 100; j++){
-        for(int i=0; i<j; i++){
-            TowerSK.insert(j);
-        }
-    }
-    int x;
-    std::cin >> x;
-    std::cout << "La frecuencia del elemento " << x << " es: " << TowerSK.estimar_freq(x) <<
-    ".\n";
-    return 0;
-}
+#include "Extraccionkmer.h"
 
 //int main (int argc, char *argv[]) {
-//    CountMin_CU  CU(5, 300);
+//    TowerSketch TowerSK(5);
 //    for(int j=0; j < 100; j++){
 //        for(int i=0; i<j; i++){
-//            CU.insertCMin(j);
+//            TowerSK.insert(j);
 //        }
 //    }
 //    int x;
 //    std::cin >> x;
-//    std::cout << "La frecuencia del elemento " << x << " es: " << CU.estimar_freq(x) <<
+//    std::cout << "La frecuencia del elemento " << x << " es: " << TowerSK.estimar_freq(x) <<
 //    ".\n";
 //    return 0;
 //}
+
+int main (int argc, char *argv[]) {
+    TowerSketch TW(5);
+    std::string archivo_csv = "resultados_totales.csv";
+    std::vector<uint64_t> kmers = leer_kmers(archivo_csv);
+    for(size_t i=0; i < kmers.size(); i++){
+        TW.insert(kmers[i]);
+        std::cout << "insertando kmer de la posición" << i << "\n";
+    }
+    std::string x;
+    std::cin >> x;
+    std::cout << "La frecuencia del elemento " << x << " es: " << TW.estimar_freq(string_to_uint64(x)) <<
+    ".\n";
+    return 0;
+}
